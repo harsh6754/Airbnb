e4821b4f394d033ffd67e62514dbf4ffc156c277
Harsh agrawal <210306105012@paruluniversity.ac.in>
2023-12-02 10:00:25 +0530

    airbnb

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -2,12 +2,14 @@
 
 import axios from 'axios';
 import { AiFillGithub } from 'react-icons/ai';
-import {FcGoogle} from 'react-icons/fc';
-import { useCallback,useState } from 'react';
-import {FieldValues,SubmitHandler,useForm} from 'react-hook-form';
+import { FcGoogle } from 'react-icons/fc';
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
+import Heading from '../Heading';
+import Input from '../input/Input';
 
 
 const RegisterModal = () => {
@@ -25,30 +27,67 @@ const RegisterModal = () => {
     },
   });
 
-  const onSubmit:SubmitHandler<FieldValues> = (data) => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
 
-    axios.post('/api/register',data)
-       .then(()=>{
+    axios.post('/api/register', data)
+      .then(() => {
         registerModal.onClose();
-       })
-       .catch((error)=>{
+      })
+      .catch((error) => {
         console.log(error);
-        
-       })
-       .finally(()=>{
+
+      })
+      .finally(() => {
         setIsLoading(false);
-       })
+      })
   }
-    return(
-      <Modal
-       disabled={isLoading}
-       isOpen={registerModal.isOpen}
-       title='Register'
-       actionLabel='Continue'
-       onClose={registerModal.onClose}
-       onSubmit={handleSubmit(onSubmit)}
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading
+        title="Welcome To Airbnb"
+        subtitle='Create an Account!'
       />
-    )
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title='Register'
+      actionLabel='Continue'
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+    />
+  )
 }
 export default RegisterModal;
